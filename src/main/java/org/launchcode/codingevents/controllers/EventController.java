ef9b552eefa443c7ffc38883756da1c2d551d712
cain9209 <zacharylcain@gmail.com>
2024-06-21 14:22:17 -0500

    Learned about fragments

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
@@ -12,18 +11,26 @@ import java.util.List;
  * Created by Chris Bay
  */
 @Controller
-@RequestMapping("events")
+@RequestMapping("/events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
-        return "events/index";
+        return "/events/index";
+    }
+
+    @GetMapping("/create")
+    public String displayCreateEventForm() {
+        return "events/create";
+    }
+
+    @PostMapping("/create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 
 }
