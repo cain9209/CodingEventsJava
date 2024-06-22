f30f0156925b4f7e2fc3887bd05fbb394d263d1a
cain9209 <zacharylcain@gmail.com>
2024-06-22 12:22:01 -0500

    Added Model and corrected Table format

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
@@ -13,23 +14,24 @@ import java.util.List;
 @Controller
 @RequestMapping("/events")
 public class EventController {
-
-    private static List<String> events = new ArrayList<>();
-
+    // create a collection to store the data which is linked to the Model//
+    private static List<Event> events = new ArrayList<>();
+// displays form at//
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("events", events);
         return "/events/index";
     }
-
+// displays create event form //
     @GetMapping("/create")
     public String displayCreateEventForm() {
         return "events/create";
     }
 
     @PostMapping("/create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName,
+                              @RequestParam String eventDescription) {
+        events.add(new Event(eventName, eventDescription));
         return "redirect:/events";
     }
 
